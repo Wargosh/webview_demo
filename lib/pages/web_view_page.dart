e381390f4e1d5d659e19733bd0c75a07b80aefe2
Wargosh <wargosh3@gmail.com>
2024-04-17 16:29:37 -0500

    version de webview en 4.2.2 Correccion ocultando teclado cuando se busca url

--- lib/pages/web_view_page.dart
+++ lib/pages/web_view_page.dart
@@ -12,31 +12,16 @@ class WebViewPage extends StatefulWidget {
 }
 
 class _WebViewPageState extends State<WebViewPage> {
-  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _textController = TextEditingController();
+  late final WebViewController _webViewController;
   String _currentUrl = '';
-  final WebViewController _webViewController = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setBackgroundColor(const Color(0x00000000))
-    ..setNavigationDelegate(
-      NavigationDelegate(
-        onProgress: (int progress) {
-          // Update loading bar
-        },
-        onPageStarted: (String url) {},
-        onPageFinished: (String url) {},
-        onWebResourceError: (WebResourceError error) {},
-        onNavigationRequest: (NavigationRequest request) {
-          if (request.url.startsWith('https://www.youtube.com/')) {
-            return NavigationDecision.prevent;
-          }
-          return NavigationDecision.navigate;
-        },
-      ),
-    )
-    ..loadRequest(Uri.parse('https://flutter.dev'));
+  bool _showAppBar = true;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+
+    // #docregion platform_features
     late final PlatformWebViewControllerCreationParams params;
     if (WebViewPlatform.instance is WebKitWebViewPlatform) {
       params = WebKitWebViewControllerCreationParams(
@@ -49,44 +34,111 @@ class _WebViewPageState extends State<WebViewPage> {
 
     final WebViewController controller =
         WebViewController.fromPlatformCreationParams(params);
-// ···
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            debugPrint('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            debugPrint('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            debugPrint('Page finished loading: $url');
+          },
+          onWebResourceError: (WebResourceError error) {
+            debugPrint('''
+Page resource error:
+  code: ${error.errorCode}
+  description: ${error.description}
+  errorType: ${error.errorType}
+  isForMainFrame: ${error.isForMainFrame}
+          ''');
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              debugPrint('blocking navigation to ${request.url}');
+              return NavigationDecision.prevent;
+            }
+            debugPrint('allowing navigation to ${request.url}');
+            return NavigationDecision.navigate;
+          },
+          onUrlChange: (UrlChange change) {
+            debugPrint('url change to ${change.url}');
+          },
+        ),
+      )
+      ..addJavaScriptChannel(
+        'Toaster',
+        onMessageReceived: (JavaScriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        },
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+
+    // #docregion platform_features
     if (controller.platform is AndroidWebViewController) {
       AndroidWebViewController.enableDebugging(true);
       (controller.platform as AndroidWebViewController)
           .setMediaPlaybackRequiresUserGesture(false);
     }
+    // #enddocregion platform_features
 
+    _webViewController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          controller: _controller,
-          keyboardType: TextInputType.url,
-          decoration: const InputDecoration(hintText: 'Enter URL here...'),
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              setState(() {
-                _currentUrl = _controller.text;
-                _webViewController.loadRequest(Uri.parse(_currentUrl));
-              });
-            },
-          ),
-          _currentUrl.isNotEmpty
-              ? IconButton(
-                  icon: const Icon(Icons.cancel_sharp),
+      resizeToAvoidBottomInset: true,
+      appBar: _showAppBar
+          ? AppBar(
+              title: TextField(
+                controller: _textController,
+                keyboardType: TextInputType.url,
+                decoration:
+                    const InputDecoration(hintText: 'Enter URL here...'),
+              ),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.search),
                   onPressed: () {
                     setState(() {
-                      _controller.clear();
-                      _currentUrl = '';
+                      _currentUrl = _textController.text;
+                      _webViewController.loadRequest(Uri.parse(_currentUrl));
+                      // _showAppBar = false;
+                      FocusManager.instance.primaryFocus?.unfocus();
                     });
                   },
-                )
-              : const SizedBox(),
-        ],
+                ),
+                _currentUrl.isNotEmpty
+                    ? IconButton(
+                        icon: const Icon(Icons.cancel_sharp),
+                        onPressed: () {
+                          setState(() {
+                            _textController.clear();
+                            _currentUrl = '';
+                          });
+                        },
+                      )
+                    : const SizedBox(),
+              ],
+            )
+          : null,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+          child: WebViewWidget(controller: _webViewController),
+        ),
       ),
-      body: WebViewWidget(controller: _webViewController),
     );
   }
 }
